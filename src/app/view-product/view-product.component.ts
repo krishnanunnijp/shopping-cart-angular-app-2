7d0f81a727569d93888191738e977e233b53d64b
krishnanunnijp <krishnanunni.j@nestgroup.net>
2022-12-08 01:54:25 +0530

    view  page added

---
+++ src/app/view-product/view-product.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
+
+@Component({
+  selector: 'app-view-product',
+  templateUrl: './view-product.component.html',
+  styleUrls: ['./view-product.component.css']
+})
+export class ViewProductComponent {
+  data :any =[]
+
+  constructor(private api:ApiService){
+
+    api.fetchProduct().subscribe(
+
+      (readValue)=>{
+        this.data=readValue
+      }
+    )
+  }
+
+}
